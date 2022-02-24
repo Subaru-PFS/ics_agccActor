9866bd2d6cef073af9c1fcc6b89010b560231b8d
Jennifer Karr <karr@HironoMacBook-Air.local>
2022-02-24 12:33:37 +0800

    rebasing and merging

--- python/agccActor/centroidTools.py
+++ python/agccActor/centroidTools.py
@@ -7,20 +7,6 @@ import sep
 from scipy.ndimage import gaussian_filter
 
 
-def getImageParams(cmd):
-
-    try:
-        cmdKeys=cmd.cmd.keywords
-    except:
-        cmdKeys=[]
-        
-    fileName=os.path.join(os.environ['ICS_AGCCACTOR_DIR'],'etc','agCamParameters.yaml')
-
-    with open(fileName, 'r') as inFile:
-        imageParms=yaml.safe_load(inFile)
-
-    return imageParms
-
 def getCentroidParams(cmd):
 
     try:
@@ -36,33 +22,26 @@ def getCentroidParams(cmd):
     #returns just the values dictionary
     centParms = defaultParms['values']
 
-    if('fwhmx' in cmdKeys):
-        centParms['fwhmx']=cmd.cmd.keywords["fwhmx"].values[0]
-    if('fwhmy' in cmdKeys):
-        centParms['fwhmy']=cmd.cmd.keywords["fwhmy"].values[0]
-
-    if('boxFind' in cmdKeys):
-        centParms['boxFind']=cmd.cmd.keywords["boxFind"].values[0]
-    if('boxCent' in cmdKeys):
-        centParms['boxCent']=cmd.cmd.keywords["boxCent"].values[0]
-
-    if('findSigma' in cmdKeys):
-        centParms['findSigma']=cmd.cmd.keywords["findSigma"].values[0]
-    if('centSigma' in cmdKeys):
-        centParms['centSigma']=cmd.cmd.keywords["centSigma"].values[0]
-    if('threshSigma' in cmdKeys):
-        centParms['threshSigma']=cmd.cmd.keywords["threshSigma"].values[0]
-
     if('nmin' in cmdKeys):
         centParms['nmin']=cmd.cmd.keywords["nmin"].values[0]
-    if('nmax' in cmdKeys):
-        centParms['nmax']=cmd.cmd.keywords["nmax"].values[0]
-    if('nmax' in cmdKeys):
-        centParms['maxIt']=cmd.cmd.keywords["maxIt"].values[0]
-
+    if('thresh' in cmdKeys):
+        centParms['fwhmy']=cmd.cmd.keywords["thresh"].values[0]
 
     return centParms
 
+def getImageParams(cmd):
+
+    try:
+        cmdKeys=cmd.cmd.keywords
+    except:
+        cmdKeys=[]
+        
+    fileName=os.path.join(os.environ['ICS_AGCCACTOR_DIR'],'etc','agCamParameters.yaml')
+
+    with open(fileName, 'r') as inFile:
+        imageParms=yaml.safe_load(inFile)
+
+    return imageParms
 
 def interpBadCol(data,badCols):
 
@@ -70,14 +49,14 @@ def interpBadCol(data,badCols):
     interpolate over bad columns
     """
 
-    w,h = data.shape()
+    w,h = data.shape
     for i in badCols:
         for j in range(h):
             data[i,j]=(data[i-1,j]+data[i+1],j)/2
     return data
 
 
-def removeOverscan(data):
+def subOverscan(data):
 
     """
     remove overscan
@@ -92,70 +71,71 @@ def removeOverscan(data):
     data[:, :w//2] -= bg0
     data[:, w//2:] -= bg1
 
-def getCentroidsSep(data,iParms,spotDtype,agcid,thresh):
+    return data
+
+def centroidRegion(data, thresh, minarea=12):
+    # determine the background
+    bgClass = sep.Background(data)
+    background = bgClass.back()
+    rms = bgClass.rms()
+    bgClass.subfrom(data)
+    
+    spots = sep.extract(data, thresh, rms, minarea = 12)
+
+    return spots,len(spots),background
+
+def getCentroidsSep(data,iParms,cParms,spotDtype,agcid):
 
     """
     runs centroiding for the sep routine and assigns the resules
 
     """
 
-    # this is needed for sep
-    from scipy.ndimage import gaussian_filter
-    
+    thresh=cParms['thresh']
+    minarea=cParms['nmin']
+
+    # get region information for camera
     region = iParms[str(int(agcid))]['reg']
 
-    data=subOverscan(data,iParms[str(int(agcid))]['reg'])
-    data=interpBadCol(data,iParms[str(int(agcid))]['badcols'])
+    data=subOverscan(data.astype('float'))
+    data=interpBadCol(data,iParms[str(int(agcid))]['badCols'])
     
     _data1 = data[region[2]:region[3],region[0]:region[1]].astype('float', copy=True)
     _data2 = data[region[6]:region[7],region[4]:region[5]].astype('float', copy=True)
 
-    # determine the background
-    bgClass1 = sep.Background(_data1)
-    background1 = bgClass1.back()
-    rms1 = bgClass1.rms()
-    bgClass1.subfrom(_data1)
+    spots1, nSpots1, background1 = centroidRegion(_data1, thresh, minarea)
+    spots2, nSpots2, background2 = centroidRegion(_data2, thresh, minarea)
 
-    spots1 = sep.extract(_data1, thresh, rms1, minarea = 10)
-
-    # determine the background
-    bgClass2 = sep.Background(_data2)
-    background2 = bgClass2.back()
-    rms2 = bgClass2.rms()
-    bgClass2.subfrom(_data2)
-
-    spots2 = sep.extract(_data2, thresh, rms2, minarea = 10)
-
-    nElem = len(spots1)+len(spots2)
+    nElem = nSpots1 + nSpots2
 
     result = np.zeros(nElem, dtype=spotDtype)
-    print(spots1['npix'])
-
-    result['image_moment_00_pix'][0:len(spots1)] = spots1['flux']
-    result['centroid_x_pix'][0:len(spots1)] = spots1['x']+region[0]
-    result['centroid_y_pix'][0:len(spots1)] = spots1['y']+region[2]
-    result['central_image_moment_20_pix'][0:len(spots1)] = spots1['x2']
-    result['central_image_moment_11_pix'][0:len(spots1)] = spots1['xy']
-    result['central_image_moment_02_pix'][0:len(spots1)] = spots1['y2']
-    result['peak_pixel_x_pix'][0:len(spots1)] = spots1['xpeak']+region[0]
-    result['peak_pixel_y_pix'][0:len(spots1)] = spots1['ypeak']+region[2]
-    result['peak_intensity'][0:len(spots1)] = spots1['peak']
-    result['background'][0:len(spots1)] = background1[spots1['ypeak'], spots1['xpeak']]
     
-    result['image_moment_00_pix'][len(spots1):nElem] = spots2['flux']
-    result['centroid_x_pix'][len(spots1):nElem] = spots2['x']+region[4]
-    result['centroid_y_pix'][len(spots1):nElem] = spots2['y']+region[6]
-    result['central_image_moment_20_pix'][len(spots1):nElem] = spots2['x2']
-    result['central_image_moment_11_pix'][len(spots1):nElem] = spots2['xy']
-    result['central_image_moment_02_pix'][len(spots1):nElem] = spots2['y2']
-    result['peak_pixel_x_pix'][len(spots1):nElem] = spots2['xpeak']+region[4]
-    result['peak_pixel_y_pix'][len(spots1):nElem] = spots2['ypeak']+region[6]
-    result['peak_intensity'][len(spots1):nElem] = spots2['peak']
-    result['background'][len(spots1):nElem] = background2[spots2['ypeak'], spots2['xpeak']]
+
+    result['image_moment_00_pix'][0:nSpots1] = spots1['flux']
+    result['centroid_x_pix'][0:nSpots1] = spots1['x']+region[0]
+    result['centroid_y_pix'][0:nSpots1] = spots1['y']+region[2]
+    result['central_image_moment_20_pix'][0:nSpots1] = spots1['x2']
+    result['central_image_moment_11_pix'][0:nSpots1] = spots1['xy']
+    result['central_image_moment_02_pix'][0:nSpots1] = spots1['y2']
+    result['peak_pixel_x_pix'][0:nSpots1] = spots1['xpeak']+region[0]
+    result['peak_pixel_y_pix'][0:nSpots1] = spots1['ypeak']+region[2]
+    result['peak_intensity'][0:nSpots1] = spots1['peak']
+    result['background'][0:nSpots1] = background1[spots1['ypeak'], spots1['xpeak']]
+    
+    result['image_moment_00_pix'][nSpots1:nElem] = spots2['flux']
+    result['centroid_x_pix'][nSpots1:nElem] = spots2['x']+region[4]
+    result['centroid_y_pix'][nSpots1:nElem] = spots2['y']+region[6]
+    result['central_image_moment_20_pix'][nSpots1:nElem] = spots2['x2']
+    result['central_image_moment_11_pix'][nSpots1:nElem] = spots2['xy']
+    result['central_image_moment_02_pix'][nSpots1:nElem] = spots2['y2']
+    result['peak_pixel_x_pix'][nSpots1:nElem] = spots2['xpeak']+region[4]
+    result['peak_pixel_y_pix'][nSpots1:nElem] = spots2['ypeak']+region[6]
+    result['peak_intensity'][nSpots1:nElem] = spots2['peak']
+    result['background'][nSpots1:nElem] = background2[spots2['ypeak'], spots2['xpeak']]
     # set flag for right half of image
-    result['flag'][0:len(spots1)] += 1
+    result['flag'][0:nSpots1] += 1
 
-    
+    return result
 
 
     
--- python/agccActor/photometry.py
+++ python/agccActor/photometry.py
@@ -5,48 +5,16 @@ import multiprocessing as mp
 import sep
 import logging
 
-spotDtype = np.dtype(dict(names=['image_moment_00_pix', 'centroid_x_pix', 'centroid_y_pix', 'central_image_moment_20_pix', 'central_image_moment_11_pix', 'central_image_moment_02_pix', 'peak_pixel_x_pix', 'peak_pixel_y_pix', 'peak_intensity', 'background', 'flags'],
-                          formats=['f4', 'f4', 'f4', 'f4', 'f4' ,'f4', 'i2', 'i2', 'f4', 'f4', 'i2']))
+spotDtype = np.dtype(dict(names=['image_moment_00_pix', 'centroid_x_pix', 'centroid_y_pix', 'central_image_moment_20_pix', 'central_image_moment_11_pix', 'central_image_moment_02_pix', 'peak_pixel_x_pix', 'peak_pixel_y_pix', 'peak_intensity', 'background', 'flags'],formats=['f4', 'f4', 'f4', 'f4', 'f4' ,'f4', 'i2', 'i2', 'f4', 'f4', 'i2']))
 
+def measure(data,agcid,cParms,iParms,cMethod,thresh=10):
 
-logger = logging.getLogger('photometry')
-logger.setLevel(logging.DEBUG)
-
-def removeOverscan(im):
-    h, w = im.shape
-    side0 = im[:, :w//2]
-    side1 = im[:, w//2:]
-    bg0 = np.median(side0[:, :4]).astype(im.dtype)
-    bg1 = np.median(side1[:, -4:]).astype(im.dtype)
-
-    im[:, :w//2] -= bg0
-    im[:, w//2:] -= bg1
-
-    return im, bg0, bg1
-
-def removeBackground(im):
-    h, w = im.shape
-    side0 = im[:, :w//2]
-    side1 = im[:, w//2:]
-    bg0 = np.median(side0).astype(im.dtype)
-    bg1 = np.median(side1).astype(im.dtype)
-
-    im[:, :w//2] -= bg0
-    im[:, w//2:] -= bg1
-
-    return im, (bg0 + bg1)/2
-
-def measure(data,cParms,cMethod,thresh=15):
     """ measure centroid positions """
-    _data = data.astype('float', copy=True)
-    try:
-        data, bg0, bg1 = removeOverscan(_data)
-    except Exception as e:
-        logger.warn(f'boom: {e}')
-
         
     if(cMethod == 'sep'):
-        result = ct.getCentroidSep(image,iParms,spotDtype,agcid):
+
+        result = ct.getCentroidSep(image,iParms,cParms,spotDtype,agcid):
+
         
     return result
 
