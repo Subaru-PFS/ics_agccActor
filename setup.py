e3877f72d50ff4d2e0568b894f7333b89c9b5389
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2022-11-14 08:58:37 -1000

    Fixing fli_camera compiling issue

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import sdss3tools
-import os.path
+import os
 import numpy
+
+os.environ['LDFLAGS']="-Wl,-O2 -Wl,--sort-common -Wl,-z,relro -Wl,"\
+    "-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,--allow-shlib-undefined "\
+    "-Wl,-rpath,/software/conda/envs/rubin3_ics/lib "\
+    "-Wl,-rpath-link,/software/conda/envs/rubin3_ics/lib -L/software/conda/envs/rubin3_ics/lib"
 
 FLI_module = Extension(
     "fli_camera",
     ["python/agccActor/fli/fli_camera.pyx"],
     library_dirs = ["c/libfli-1.999.1-180223"],
-    libraries = ["fli", "usb-1.0"],
+    libraries = ["usb-1.0","fli"],
     include_dirs = ["c/libfli-1.999.1-180223",
                     "python/agccActor/fli",
                     numpy.get_include()],
