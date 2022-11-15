564661340596e17582a15c9cef010d7018129a89
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2022-11-14 17:26:33 -1000

    Change to the correct ocompiling order

--- setup.py
+++ setup.py
@@ -4,16 +4,12 @@ import sdss3tools
 import os
 import numpy
 
-os.environ['LDFLAGS']="-Wl,-O2 -Wl,--sort-common -Wl,-z,relro -Wl,"\
-    "-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,--allow-shlib-undefined "\
-    "-Wl,-rpath,/software/conda/envs/rubin3_ics/lib "\
-    "-Wl,-rpath-link,/software/conda/envs/rubin3_ics/lib -L/software/conda/envs/rubin3_ics/lib"
 
 FLI_module = Extension(
     "fli_camera",
     ["python/agccActor/fli/fli_camera.pyx"],
     library_dirs = ["c/libfli-1.999.1-180223"],
-    libraries = ["usb-1.0","fli"],
+    libraries = ["fli","usb-1.0"],
     include_dirs = ["c/libfli-1.999.1-180223",
                     "python/agccActor/fli",
                     numpy.get_include()],
