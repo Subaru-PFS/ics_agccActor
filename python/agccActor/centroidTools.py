06ea4383e7010b6db84e9a7040ffcca6f8ec988b
JenniferKarr <jkarr@asiaa.sinica.edu.tw>
2022-12-15 17:26:28 +0800

    updating template fitting after testing

--- python/agccActor/centroidTools.py
+++ python/agccActor/centroidTools.py
@@ -8,8 +8,10 @@ from scipy.ndimage import gaussian_filter
 from scipy.integrate import dblquad
 from lmfit import Model
 import lmfit
-import dbRoutinesAGCC as dbTools
-
+from agccActor import dbRoutinesAGCC as dbTools
+from scipy import signal
+from scipy.optimize import curve_fit
+from astropy.io import fits
 
 def getCentroidParams(cmd):
 
@@ -41,43 +43,49 @@ def updateTemplate(cmd,centParms):
     load a pair of model PSF templates
     """
 
-    
+    try:
+        cmdKeys=cmd.cmd.keywords
+    except:
+        cmdKeys=[]
+
     #default value for unfocussed images
-    dZ="+0.58"
+    dZ=0.58
 
 
     # we can choose a model from an explicitly given dZ
     if('dz' in cmdKeys):
         dZ=float(cmd.cmd.keywords["dz"].values[0])
 
-        #get the dZ value for the closest model.
-        #Negative and positive have different binning
-        #if the value is larger/smaller than range, set to the edge,
+    #get the dZ value for the closest model.
+    #Negative and positive have different binning
+    #if the value is larger/smaller than range, set to the edge,
 
-        if(dZ > 0.98):
-            dZString = "+0.98"
-        elif(dZString < -0.9):
-            dZString = "-0.90"
-        elif(dZ > 0):
-            dZString=f'{round(dZ/0.02)*0.02:+.2f}'
-        else:
-            dZString=f'{round(dZ/0.2)*0.2:+.2f}'
+    print(dZ)
+    if(dZ > 0.98):
+        dZString = "+0.98"
+    elif(dZ < -0.9):
+        dZString = "-0.90"
+    elif(dZ > 0):
+        dZString=f'{round(dZ/0.02)*0.02:+.2f}'
+    else:
+        dZString=f'{round(dZ/0.2)*0.2:+.2f}'
 
     
     fName=os.path.join(os.environ['PFS_INSTDATA_DIR'],'data','agc','psfTemplates',f'dZ{dZString}_SS0.70.fits')
 
     templateR = fits.getdata(fName)[0:81,:]
     templateL = fits.getdata(fName)[81:162,:]
-    self.cParms['templateR'] = templateR
-    self.cParms['templateL'] = templateL
-
+    centParms['templateR'] = templateR
+    centParms['templateL'] = templateL
+    centParms['dZ'] = dZ
+    
     #larger gridsize for very unfocused images
     if(np.abs(dZ) > 0.4):
-        self.cParms['gridSize']=31
+        centParms['gridSize']=31
     else:
-        self.cParms['gridSize']=21
+        centParms['gridSize']=21
 
-    return cParms,fName
+    return centParms,fName
 
 def loadTemplate(focus,seeing):
     
@@ -86,8 +94,6 @@ def loadTemplate(focus,seeing):
 
     templateR = fits.getdata(fName)[0:81,:]
     templateL = fits.getdata(fName)[81:162,:]
-    #templateR = np.flipud(fits.getdata(fName)[0:81,:])
-    #templateL = np.flipud(fits.getdata(fName)[81:162,:])
 
     return templateL, templateR   
 
@@ -151,17 +157,16 @@ def getCentroidsTem(data,iParms,cParms,spotDtype,agcid):
     runs centroiding for the sep routine and assigns the results
     """
 
-    refFrame = cParms['refFrame']
     templateL = cParms['templateL']
     templateR = cParms['templateR']
     gridSize = cParms['gridSize']
 
     # get the reference positions for this camera ?? check 0/1 counting
     
-    agNo = cParms['refpos'].values
-    ind=np.where(agNo == agcid+1)
-    xPos = cParms['refpos']['centroid_x_pix'].values[ind]
-    yPos = cParms['refpos']['centroid_y_pix'].values[ind]
+    #agNo = cParms['refpos']
+    #ind=np.where(agNo == agcid+1)
+    xPos = cParms['refpos']['centroid_x_pix']
+    yPos = cParms['refpos']['centroid_y_pix']
     
     # get region information for camera
     region = iParms[str(agcid + 1)]['reg']
@@ -171,57 +176,74 @@ def getCentroidsTem(data,iParms,cParms,spotDtype,agcid):
     dataProc=subOverscan(data.astype('float'))
     dataProc=interpBadCol(dataProc,iParms[str(agcid + 1)]['badCols'])
 
+    # process the images and subtract the backgrounds
     _data1 = dataProc[region[2]:region[3],region[0]:region[1]].astype('float', copy=True, order="C")
     _data2 = dataProc[region[6]:region[7],region[4]:region[5]].astype('float', copy=True, order="C")
+    bgClass1 = sep.Background(_data1)
+    background1 = bgClass1.back()
+    bgClass2 = sep.Background(_data2)
+    background2 = bgClass2.back()
+    
     newData = dataProc.copy()
     newData[region[2]:region[3],region[0]:region[1]]-=background1
     newData[region[6]:region[7],region[4]:region[5]]-=background2
 
-
+    # array for the results. It's set to the number of reference points
     nElem = len(xPos)
     result = np.zeros(nElem, dtype=spotDtype)
 
-    #first, get the new positions (also flags for saturation and position, and background position)
+    fx=5
+    fy=5
+    #first, get the new positions (also flags for saturation and position, background value and peak_intensity values
+    
     for k in range(len(xPos)):
+
+        # two templates, for glass and non glass
         xP = xPos[k]
         yP = yPos[k]
         if(xP <= 536):
-            newPos, pPos, pVal, sat = fitTemplate(templateL, newData, (xP,yP), gridSize)
-            result['background'][k] = background1[pPos[1]-region[2], pPos[0]-region[0]]
+            newPos, pPos, pVal, flag = fitTemplate(templateL, newData, (xP,yP), gridSize)
+            result['background'][k] = background1[int(yP)-region[2], int(xP)-region[0]]
 
+            #edge flag
             if(np.any([newPos[0]-2*fx < 0, newPos[1]+2*fx > (region[1]-region[0]),newPos[1]-2*fy < 0, newPos[1]+2*fy > (region[3]-region[2])],axis=0)):
                 result['flags'][k] += 2
 
 
         else:
-            newPos, pPos, pVal, sat = fitTemplate(templateR, newData, (xP,yP), gridSize)
-            result['background'][k] = background2[pPos[1]-region[6], pPos[0]-region[4]]
+            newPos, pPos, pVal, flag = fitTemplate(templateR, newData, (xP,yP), gridSize)
+            result['background'][k] = background2[int(yP)-region[6], int(xP)-region[4]]
+            
 
-            results['flags'][k]+=1
+            #side flag
+            result['flags'][k]+=1
+            
+            #edge flag
             if(np.any([newPos[0]-2*fx < 0, newPos[1]+2*fx > (region[5]-region[4]),newPos[1]-2*fy < 0, newPos[1]+2*fy > (region[7]-region[6])],axis=0)):
                 result['flags'][k] += 2
 
+        #update the array
         result['centroid_x_pix'][k]=newPos[0]
         result['centroid_y_pix'][k]=newPos[1]
-        result['flags'][k] += sat
+        result['flags'][k] += flag
         result['peak_pixel_x_pix'][k]=pPos[0]
         result['peak_pixel_x_pix'][k]=pPos[1]
-        result['peak_intensity'] = pPos
+        result['peak_intensity'] = pVal
+
+        
 
         # now the windowed second moments; need to think about what values when this doesn't converge??
         xv,yv, xyv, conv = windowedFWHM(newData, newPos[0], newPos[1])
         if(conv == 0):
-            result['image_central_moment_02_pix'][k]=xv
-            result['image_central_moment_20_pix'][k]=yv
-            result['image_central_moment_11_pix'][k]=xyv
+            result['central_image_moment_02_pix'][k]=xv
+            result['central_image_moment_20_pix'][k]=yv
+            result['central_image_moment_11_pix'][k]=xyv
         else:
-            result['image_central_moment_02_pix'][k]=0
-            result['image_central_moment_20_pix'][k]=0
-            result['image_central_moment_11_pix'][k]=0
+            result['central_image_moment_02_pix'][k]=0
+            result['central_image_moment_20_pix'][k]=0
+            result['central_image_moment_11_pix'][k]=0
         result['flags'][k] += conv
             
-        # add flag for non converged sources
-        flags.append(conv)
 
     #result['image_moment_00_pix'][nSpots1:nElem] = spots2['flux']
     return result
@@ -238,9 +260,6 @@ def getCentroidsSep(data,iParms,cParms,spotDtype,agcid):
     minarea=cParms['nmin']
     deblend=cParms['deblend']
 
-    xPos, yPos = readSpotPos(db, frameId)
-        
-
     # get region information for camera
     region = iParms[str(agcid + 1)]['reg']
     satValue = iParms['satVal']
@@ -252,18 +271,18 @@ def getCentroidsSep(data,iParms,cParms,spotDtype,agcid):
     _data1 = dataProc[region[2]:region[3],region[0]:region[1]].astype('float', copy=True, order="C")
     _data2 = dataProc[region[6]:region[7],region[4]:region[5]].astype('float', copy=True, order="C")
 
+    spots1, nSpots1, background1 = centroidRegion(_data1, thresh, minarea=minarea, deblend=deblend)
+    spots2, nSpots2, background2 = centroidRegion(_data2, thresh, minarea=minarea, deblend=deblend)
 
+    
+    nElem = nSpots1 + nSpots2
     result = np.zeros(nElem, dtype=spotDtype)
 
     # flag spots near edge of region
 
     # dynamic fwhm calculation is overenthusiastic with out of focus images
-    #fx = spots1['x2'].mean()
-    #fy = spots1['y2'].mean()
-
-    fx = 5
-    fy = 5
-    
+    fx=5
+    fy=5
     ind1 = np.where(np.any([spots1['x']-2*fx < 0, spots1['x']+2*fx > (region[1]-region[0]),spots1['y']-2*fy < 0, spots1['y']+2*fy > (region[3]-region[2])],axis=0))
     #ind2 = spots1['peak'] == satValue
     
@@ -524,21 +543,18 @@ def fitTemplate(template, data, starPos, gridSize=21):
     input: 
         template: numpy array with template that matches the seeing and focus offset of the exposure
         data: input image
-        starPos:  position of star from focussed image
+        starPos:  position of star from focussed image (pixels)
         gridSize: size of grid to compute x-corr over
     
     output: 
         newPos: updated position
         sat: flag for saturated star
-
-    note that the template is assumed to be square
-    
     """
-
-    # get the size of the template (assumes square)
+    
+    # get the size of the t
     tSize = template.shape[0]
     
-    # half point of template and grid
+    # half point of template/grid
     g2 = gridSize // 2
     t2 = tSize // 2
    
@@ -548,26 +564,77 @@ def fitTemplate(template, data, starPos, gridSize=21):
 
     # subimage centred on the star position
     dSub = data[ycI-g2:ycI+g2+1,xcI-g2:xcI+g2+1]
-    
+
+    flag = 0
+
     # do the correlation
-    # the built in python cross correlation routine turns out to
-    # be pretty fast, as the underlying code is in C
-    # mode = same gives us an output image the same size as the input
-    
-    sumVal=signal.correlate2d (dSub, template, mode='same')
-            
+    sumVal=signal.correlate2d(dSub, np.flipud(np.fliplr(template)),mode='same')
+    # if we don't have enough pixels to do the correlation, return original position, set flag
+    if(np.array(dSub.shape).min()==0):
+        newPos = starPos
+        flag = flag + 32
+        return newPos, [0,0], 0, flag
+
+    # get the position and value of the maximum pixel
+    pPos = np.array(np.unravel_index(dSub.argmax(),(gridSize,gridSize)))+np.array([g2,g2])
+    pVal = dSub.max()
+
     #find the offset of the max value from the centre of the grid
     offset = np.array(np.unravel_index(sumVal.argmax(),(gridSize,gridSize)))-g2
+    offseta = np.array(np.unravel_index(sumVal.argmax(),(gridSize,gridSize)))
 
-    #and the corresponding new position
-    newPos = starPos - np.array([offset[1],offset[0]])
+    # now we apply a sub-pixel parabolic interpolation over the centre of the
+    # cross correlation function to find the true maximum.
+
+
+
+    l1 = -0
+    l2 = +0
+
+    i1=0
+    i2=1
+
+    yy=np.zeros((sumVal.shape[0]))
+    for i in range(l1,l2+1):
+        yy = yy+ sumVal[:,offseta[i1]+i]
+    xx = np.arange(-g2,g2+1)
+
+    fflag=0
+    # try/except is because curve_fit crashes if it doesn't converge
+    try:
+        popt,pcov = curve_fit(parabola,xx[offseta[i1]-2:offseta[i1]+2+1], yy[offseta[i1]-2:offseta[i1]+2+1],p0=[-5,0.1,1e3])
+        cX = popt[1]
+    except:
+        cX = offset[0]
+        fflag=64
+        
+        
+    yy=np.zeros((sumVal.shape[1]))
+    for i in range(l1,l2+1):
+        yy = yy+ sumVal[offseta[i2]+i,:]
     
-    # check for saturation
-    if(dSub.max()==65535):
-        sat=1
-    else:
-        sat=0
+    xx = np.arange(-g2,g2+1)
 
-    #position and value of the peak position
-    peakPix = np.unravel_index(dSub.argmax(),dSub.shape)+np.array([g2,g2])
-    return newPos, peakPix, dSub.max(),sat
+    try:
+        popt,pcov = curve_fit(parabola,xx[offseta[i2]-2:offseta[i2]+3], yy[offseta[i2]-2:offseta[i2]+3],p0=[-5,0.1,1e3])
+        
+        cY = popt[1]
+    except:
+        cY = offset[1]
+        fflag=64
+
+    #set flag if the sub-pixel interpolation didn't work
+    flag = flag + fflag
+
+
+    #apply the offset
+    offset=[cX,cY]
+    newPos = starPos + np.array([offset[1],offset[0]])
+
+
+    return newPos, pPos, pVal, flag
+
+           
+def parabola(x,a,b,c):
+
+    return a*(x-b)**2+c
