0ec460d42df5f0b334002cf72a27b8b90b1111e0
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2023-06-24 00:41:00 -1000

    Fixing typo

--- python/agccActor/fli/fli_camera.pyx
+++ python/agccActor/fli/fli_camera.pyx
@@ -58,7 +58,7 @@ class CameraInit:
         if FLIGetLibVersion(libver, LIBVERSIZE) != 0:
             raise FliError("FLIGetLibVersion failed")
 
-c        EnumerateCameras()
+        EnumerateCameras()
 
 
 
